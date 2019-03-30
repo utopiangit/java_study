9a869f579cbf4ba8887280ecf7dbf66009086a09
utopian <wuthering1118@gmail.com>
2019-03-30 17:37:39 +0900

    initial commit and prices

---
+++ Test/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello world!");
+
+		int N = 10;
+		for (int i = 0; i < N; ++i )
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
