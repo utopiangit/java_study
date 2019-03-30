a601003ed37cec4107295a289a08e0fba45607e7
utopian <wuthering1118@gmail.com>
2019-03-30 18:15:24 +0900

    generalize prices

--- Test/src/Prices/Prices.java
+++ Test/src/Prices/Prices.java
@@ -18,15 +18,15 @@ public class Prices {
 
 	public static int maximum_profit(int n, int[] prices)
 	{
-		int[][] dp = new int[n][2];
+		int[][] dp = new int[n + 1][2];
 		dp[0][0] = 0;
-		dp[0][1] = -prices[0];
-		for (int i = 1; i < n; ++i)
+		dp[0][1] = -inf();
+		for (int i = 1; i <= n; ++i)
 		{
-			dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
-			dp[i][1] = Math.max(dp[i-1][0] - prices[i], dp[i-1][1]);
+			dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i - 1]);
+			dp[i][1] = Math.max(dp[i-1][0] - prices[i - 1], dp[i-1][1]);
 		}
-		return dp[n-1][0];
+		return dp[n][0];
 	}
 
 	public static int inf()
